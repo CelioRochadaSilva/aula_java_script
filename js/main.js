22ce041e8419101454327c270570a2a2aa8b60ed
CelioRochadaSilva <celioelea@hotmail.com>
2021-02-10 15:54:44 -0300

    mais exemplos
    mais exemplos

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//var nome = "Celio Rocha";
+//var idade = 50;
+//var idade2 = 1;
+//var frase = "Nautico saco de pancada do Santa Cruz";
+ /* alert (nome + " tem " + idade + " anos "); */
+// alert (idade + idade2); // exibe somatoria idade mais idade2
+
+//console.log (nome);
+//console.log(idade + idade2);
+//console.log(frase.replace ("Santa Cruz", "Sport")); // substituindo Santa Cruz por Sport
+//alert (frase.replace ("Santa Cruz", "Sport")); // substituindo Santa Cruz por Sport
+//console.log(frase.toUpperCase()); // vai exibir toda frazer em maiuscula
+
+//console.log(frase.toLowerCase()); //vai exibir toda frazer em minuscula
+//var lista = ["maçã" , "pera" , "Uva"]
+//lista.push("Banana"); // vai adicionar Banana na lista do array.
+//lista.pop("pera"); // vai remover item pera da lista
+//console.log(lista); //vai exibi todo lista.
+//console.log(lista.length); // exibe quantidade de itens da lista
+//console.log(lista.reverse()); // exibe os itens sequencia contraria 
+//console.log(lista.toString()[1]); // exibe itens formato string ou seja exibe a: por que segundo iten da lista
+//console.log(lista.join("- ")); // vai separa item lista com um -
+
+/*var fruta = {nome: "maçã" , cor: "vermelha" } // exemplo uso dicionario
+console.log(fruta.nome); // vai exibinoe da fruta: maçã
+alert(fruta.cor); //vai exibir alert de cor da maçã */
+
+//var frutas =[ {nome: "maçã" , cor: "vermelha" },{ nome: "pera" , cor: "verde" }] // exemplo uso lista de dicionario
+//console.log(frutas[0].nome); // vai exibinoe da fruta: maçã
+//alert(frutas[1].cor); //vai exibir alert de cor da maçã
+var idade = 18;
+if (idade > 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+
+}
+console.log(idade)
